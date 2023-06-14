6650cb4ba6aea8530b9163eefbbd34622cfdf1f4
Moatasem Sorour <moatasem.sorour@gmail.com>
2023-06-13 23:13:45 -0400

    add nav bar

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,18 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
+    
     <div className="container">
+      <body>
+        <nav>
+            <ul class="navbar">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#portfolio">Portfolio</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+      </body> 
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
